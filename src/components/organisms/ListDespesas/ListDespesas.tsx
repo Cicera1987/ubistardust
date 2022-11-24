9008230da52efb14171ee8fab6696ef127f5fa07
Cicera Ribeiro <maccicera@gmail.com>
2022-11-24 14:44:41 -0300

    inserindo despesas

--- src/components/organisms/ListDespesas/ListDespesas.tsx
+++ src/components/organisms/ListDespesas/ListDespesas.tsx
@@ -1,14 +1,55 @@
 import { Typography } from '@stardust-ds/react'
+import {
+  ContainerDados,
+  ContainerHeader,
+  ContainerGlobal,
+} from "../../pages/Home/style";
+import { DataDespesas } from '../../style/type/type';
 
 
 const ListDespesas = () => {
+  const menuDespesas: DataDespesas[] = [
+    {
+      id: 1,
+      name: "Consultoria Financeira",
+      price: 1200
+    },
+    {
+      id: 1,
+      name: "Consultoria Fiscal Cont",
+      price: 1390
+    },
+    {
+      id: 1,
+      name: "Consultoria Jurídica Dir",
+      price: 2000
+    },
+  ];
+
+
   return (
-    <div >
-      <Typography type="h2" color="#ccc">
-        Despesas
-      </Typography>
-    </div>
-  )
+    <>
+      <ContainerDados>
+        <Typography type="h2" color="#ccc">
+          Despesas
+        </Typography>
+        <ContainerHeader>
+          <Typography type="h2" color="#ccc">
+            Tipo de Serviço prestado
+          </Typography>
+          <Typography type="h2" color="#ccc">
+            Valor do serviço
+          </Typography>
+        </ContainerHeader>
+        {menuDespesas.map((item) => (
+          <ContainerGlobal key={item.id}>
+            <div style={{ paddingRight: "10em" }}>{item.name}</div>
+            <div style={{ paddingRight: "6em" }}>R$ {item.price},00</div>
+          </ContainerGlobal>
+        ))}
+      </ContainerDados>
+    </>
+  );
 };
 
 export default ListDespesas
